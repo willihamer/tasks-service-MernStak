82d8e141cd557654ff27abf8b519bff78de98dc7
Hamer Patarroyo <hamer.patarroyo@gmail.com>
2021-04-24 19:39:14 -0500

    new changes

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-})
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// })
 
 
 // ENABLE EXPRESS.JSON
